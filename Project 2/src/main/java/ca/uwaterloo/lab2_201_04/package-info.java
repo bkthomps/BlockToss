369914fa84246368fbfbef0e0629e77f0128522b
bkthomps <bkthomps@edu.uwaterloo.ca>
2019-04-09 23:55:17 -0700

    Remove non-final project

--- Project 2/src/main/java/ca/uwaterloo/lab2_201_04/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Android app which displays accelerometer direction.
- * <p>
- * Displays an acceleration graph, as well as displays the direction of motion. Also, user can save acceleration data.
- *
- * @see ca.uwaterloo.sensortoy.LineGraphView
- */
-package ca.uwaterloo.lab2_201_04;
